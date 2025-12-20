3d7d3fadd8adba1b05fca90007ddd94248079f4d
WolfGangS <flamin2k8@gmail.com>
2025-12-20 18:37:58 +0000

    Reduce config switches for meta output, seperating using it for loading and outputing it causes confusion
    Signed-off-by: WolfGangS <flamin2k8@gmail.com>

--- src/interfaces/configinterface.ts
+++ src/interfaces/configinterface.ts
@@ -32,7 +32,6 @@ export enum ConfigKey {
   CompareHashBeforeSync = 'sync.compareHashBeforeSync',
 
   FileMetaInfoInOutput ='sync.includeFileMetaInOutput',
-  FileMetaInfoUseForMatching ='sync.useFileMetaForMatching',
   FileMetaInfoIncludeCreator ='sync.includeCreatorInFileMeta',
 }
 
